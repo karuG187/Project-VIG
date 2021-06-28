c8942c691225c0dc3bc26b2907d8bc5bd421d69a
karuG187 <59677491+karuG187@users.noreply.github.com>
2021-06-28 20:00:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("lol");
+alert("AA");
 window.onload = () => {
   const video  = document.querySelector("#camera");
   const canvas = document.querySelector("#picture");
@@ -20,7 +20,7 @@ window.onload = () => {
   /**
    * カメラを<video>と同期
    */
-  /*
+  
   navigator.mediaDevices.getUserMedia(constraints)
   .then( (stream) => {
     video.srcObject = stream;
@@ -31,21 +31,21 @@ window.onload = () => {
   .catch( (err) => {
     console.log(err.name + ": " + err.message);
   });
-  */
+  
 
   /**
    * シャッターボタン
    */
    document.querySelector("#shutter").addEventListener("click", () => {
     const ctx = canvas.getContext("2d");
-
+    /*
     // 演出的な目的で一度映像を止めてSEを再生する
     video.pause();  // 映像を停止
     se.play();      // シャッター音
     setTimeout( () => {
       video.play();    // 0.5秒後にカメラ再開
     }, 500);
-
+    */
     // canvasに画像を貼り付ける
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
   });
