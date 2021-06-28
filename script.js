1892eae4bbdfeded3df8ac3ae0c06a00456a392b
karuG187 <59677491+karuG187@users.noreply.github.com>
2021-06-28 19:03:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("VIGVIG");
+const medias = {
+  audio: false,
+  video: {
+    facingMode: {
+      exact: "environment" // リアカメラにアクセス
+    }
+  }
+};
+const video = document.getElementById("video");
+const promise = navigator.mediaDevices.getUserMedia(medias);
+
+promise.then(successCallback)
+       .catch(errorCallback);
+
+function successCallback(stream) {
+  video.srcObject = stream;
+};
+
+function errorCallback(err) {
+  alert(err);
+};
